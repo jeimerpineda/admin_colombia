6e666a8827b82db5d92b69096b7569e259df14e6
Emerson Ramirez <ramirezemerson1991@gmail.com>
2019-01-30 11:22:21 -0400

    Cambios para mejorar el diseño responsive

--- resources/views/tiposfactura/update.blade.php
+++ resources/views/tiposfactura/update.blade.php
@@ -2,7 +2,7 @@
 @section('title') Actualizar Tipos de Factura @endsection
 @section('content')
 	{!!\Basics::Breadcrumb(['Configuración','Tipos de Factura','Actualizar'])!!}
-	<div class="col-8 offset-2">
+	<div class="col-xs-6 col-md-6 offset-md-3 offset-xs-3">
 		<div class="card">
 			<div class="card-header">
 				Actualizar Tipos de Factura
@@ -10,17 +10,17 @@
 			<div class="card-body">
 				{!!\Basics::printMessage('message')!!}
 				{!!\Basics::printErrors($errors->any(),$errors->all())!!}
-				<form action="{{route('config.tiposdefacturas.update.form')}}" id="form_tipfacturas" method="POST" class="col-8 offset-2">
+				<form action="{{route('config.tiposdefacturas.update.form')}}" id="form_tipfacturas" method="POST" class="col-md-8 col-xs-6 col-form-label offset-md-2 offset-xs-3">
 					@csrf
 					<div class="form-group row">
-						<label for="descripcion" class="col-3 col-form-label">Descripción:</label>
-						<div class="col-9">
+						<label for="descripcion" class="col-md-4 col-xs-12 col-form-label">Descripción:</label>
+						<div class="col-md-8 col-xs-12">
 							<input required type="text" name="descripcion" class="form-control" value="{{$tiposdefacturas->descripcion}}">
 						</div>
 					</div>
 					<div class="form-group row">
-						<label for="status" class="col-2 col-form-label">Status:</label>
-						<div class="col-10">
+						<label for="status" class="col-md-4 col-xs-12 col-form-label">Status:</label>
+						<div class="col-md-8 col-xs-12 col-form-label">
 							<select name="status" id="status" class="custom-select">
 								<option value="1" {{\Basics::selected(1,$tiposdefacturas->status)}}>Activo</option>
 								<option value="0" {{\Basics::selected(0,$tiposdefacturas->status)}}>Inactivo</option>
