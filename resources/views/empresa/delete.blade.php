63196158673dcf7b310f51e3161db3ad6540250e
Emerson Ramirez <ramirezemerson1991@gmail.com>
2019-01-30 13:06:38 -0400

    Cambios para mejorar el diseño responsive Empresa

--- resources/views/empresa/delete.blade.php
+++ resources/views/empresa/delete.blade.php
@@ -2,7 +2,7 @@
 @section('title') Eliminar Empresa @endsection
 @section('content')
 	{!!\Basics::Breadcrumb(['Configuración','Empresa','Eliminar'])!!}
-	<div class="col-8 offset-2">
+	<div class="col-xs-6 col-md-10 offset-md-1 offset-xs-3">
 		<div class="card">
 			<div class="card-header">
 				Eliminar Empresa
@@ -11,50 +11,54 @@
 				<div class="alert alert-info">
 					Esta acción borrará el registro de la base de datos. ¿Está segur@ que desea continuar?
 				</div>
-				<form action="{{route('config.empresa.delete.form')}}" id="form_empresa" method="POST" class="col-8 offset-2">
+				<form action="{{route('config.empresa.delete.form')}}" id="form_empresa" method="POST" class="col-12">
 					@csrf
-					<div class="form-group row">
-						<label for="nit" class="col-3 col-form-label">Nit:</label>
-						<div class="col-9">
-							<input required type="text" name="nit" class="form-control" value="{{$listempresa->nit}}">
+				<div class="form-row">
+					<div class="form-group col-12 col-md-4">
+						<label for="nit" class="col-12 col-form-label">Nit:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="nit" class="form-control" value="{{$listempresa->nit}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="razon" class="col-3 col-form-label">Razon Social:</label>
-						<div class="col-9">
-							<input required type="text" name="razon" class="form-control" value="{{$listempresa->razon_social}}">
+					<div class="form-group col-12 col-md-4">
+						<label for="razon" class="col-12 col-form-label">Razon Social:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="razon" class="form-control" value="{{$listempresa->razon_social}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="direccion" class="col-3 col-form-label">Direccion:</label>
-						<div class="col-9">
-							<input required type="text" name="direccion" class="form-control" value="{{$listempresa->direccion}}">
+					<div class="form-group col-12 col-md-4">
+						<label for="direccion" class="col-12 col-form-label">Direccion:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="direccion" class="form-control" value="{{$listempresa->direccion}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="correo" class="col-3 col-form-label">Correo:</label>
-						<div class="col-9">
-							<input required type="text" name="correo" class="form-control" value="{{$listempresa->correo}}">
+					<div class="form-group col-12 col-md-4">
+						<label for="correo" class="col-12 col-form-label">Correo:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="correo" class="form-control" value="{{$listempresa->correo}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="tlf1" class="col-3 col-form-label">Telefono Principal:</label>
-						<div class="col-9">
-							<input required type="text" name="tlf1" class="form-control" value="{{$listempresa->telefono_principal}}">
+					<div class="form-group col-12 col-md-4">
+						<label for="tlf1" class="col-12 col-form-label">Telefono Principal:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="tlf1" class="form-control" value="{{$listempresa->telefono_principal}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="tlf2" class="col-3 col-form-label">Telefono Segundario:</label>
-						<div class="col-9">
-							<input type="text" name="tlf2" class="form-control" value="{{$listempresa->telefono_segundario}}">
+					<div class="form-group col-12 col-md-4">
+						<label for="tlf2" class="col-12 col-form-label">Telefono Segundario:</label>
+						<div class="col-12">
+							<input disabled type="text" name="tlf2" class="form-control" value="{{$listempresa->telefono_segundario}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="sucursal" class="col-3 col-form-label">Sucursal:</label>
-						<div class="col-9">
-							<input required type="text" name="sucursal" class="form-control" value="{{$listempresa->sucursal}}">
+				</div>
+				<div class="form-row">
+					<div class="form-group col-12 col-md-4">
+						<label for="sucursal" class="col-12 col-form-label">Sucursal:</label>
+						<div class="col-12">
+							<input disabled required type="text" name="sucursal" class="form-control" value="{{$listempresa->sucursal}}">
 						</div>
 					</div>
+				</div>
 					<div class="btn-group d-flex justify-content-center">
 						<a href="{{route('config.empresa')}}" class="btn btn-link mr-2">
 							<i class="fa fa-arrow-left"></i> Volver
--- resources/views/empresa/update.blade.php
+++ resources/views/empresa/update.blade.php
@@ -2,7 +2,7 @@
 @section('title') Actualizar Empresa @endsection
 @section('content')
 	{!!\Basics::Breadcrumb(['Configuración','Empresa','Actualizar'])!!}
-	<div class="col-8 offset-2">
+	<div class="col-xs-6 col-md-10 offset-md-1 offset-xs-3">
 		<div class="card">
 			<div class="card-header">
 				Actualizar Empresa
@@ -10,51 +10,55 @@
 			<div class="card-body">
 				{!!\Basics::printMessage('message')!!}
 				{!!\Basics::printErrors($errors->any(),$errors->all())!!}
-				<form action="{{route('config.empresa.update.form')}}" id="form_tipfacturas" method="POST" class="col-8 offset-2">
+				<form action="{{route('config.empresa.update.form')}}" id="form_tipfacturas" method="POST" class="col-12">
 					@csrf
-					<div class="form-group row">
-						<label for="nit" class="col-3 col-form-label">Nit:</label>
-						<div class="col-9">
+					<div class="form-row">
+					<div class="form-group col-12 col-md-4">
+						<label for="nit" class="col-12 col-form-label">Nit:</label>
+						<div class="col-12">
 							<input required type="text" name="nit" class="form-control" value="{{$listempresa->nit}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="razon" class="col-3 col-form-label">Razon Social:</label>
-						<div class="col-9">
+					<div class="form-group col-12 col-md-4">
+						<label for="razon" class="col-12 col-form-label">Razon Social:</label>
+						<div class="col-12">
 							<input required type="text" name="razon" class="form-control" value="{{$listempresa->razon_social}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="direccion" class="col-3 col-form-label">Direccion:</label>
-						<div class="col-9">
+					<div class="form-group col-12 col-md-4">
+						<label for="direccion" class="col-12 col-form-label">Direccion:</label>
+						<div class="col-12">
 							<input required type="text" name="direccion" class="form-control" value="{{$listempresa->direccion}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="correo" class="col-3 col-form-label">Correo:</label>
-						<div class="col-9">
+					<div class="form-group col-12 col-md-4">
+						<label for="correo" class="col-12 col-form-label">Correo:</label>
+						<div class="col-12">
 							<input required type="text" name="correo" class="form-control" value="{{$listempresa->correo}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="tlf1" class="col-3 col-form-label">Telefono Principal:</label>
-						<div class="col-9">
+					<div class="form-group col-12 col-md-4">
+						<label for="tlf1" class="col-12 col-form-label">Telefono Principal:</label>
+						<div class="col-12">
 							<input required type="text" name="tlf1" class="form-control" value="{{$listempresa->telefono_principal}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="tlf2" class="col-3 col-form-label">Telefono Segundario:</label>
-						<div class="col-9">
+					<div class="form-group col-12 col-md-4">
+						<label for="tlf2" class="ol-12 col-form-label">Telefono Segundario:</label>
+						<div class="col-12">
 							<input type="text" name="tlf2" class="form-control" value="{{$listempresa->telefono_segundario}}">
 						</div>
 					</div>
-					<div class="form-group row">
-						<label for="sucursal" class="col-3 col-form-label">Sucursal:</label>
-						<div class="col-9">
+				</div>
+				<div class="form-row">
+					<div class="form-group col-12 col-md-4">
+						<label for="sucursal" class="col-12 col-form-label">Sucursal:</label>
+						<div class="col-12">
 							<input required type="text" name="sucursal" class="form-control" value="{{$listempresa->sucursal}}">
 						</div>
 					</div>
-					
+				</div>
+
 					<div class="btn-group d-flex justify-content-center">
 						<a href="{{route('config.empresa')}}" class="btn btn-link mr-2">
 							<i class="fa fa-arrow-left"></i> Volver
